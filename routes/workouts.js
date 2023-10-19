90bd29860ff80bc2b093d35a17dc667b1538eed4
iamemmanuel <fsdevmanuel@gmail.com>
2023-10-19 18:19:41 +0100

    backend

---
+++ routes/workouts.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const {
+    createWorkout, 
+    getWorkouts,
+    getWorkout,
+    deleteWorkout,
+    updateWorkout
+
+} = require('../controllers/workoutController')
+const requireAuth = require('../middleware/requireauth')
+
+//require auth for all workout routes
+const router = express.Router()
+
+router.use(requireAuth)
+
+//GET all workouts
+router.get('/', getWorkouts)
+
+//GET a single workout
+router.get('/:id', getWorkout)  
+
+//POST a new workout  
+router.post('/', createWorkout)
+
+//DELETE a new workout  
+router.delete('/:id', deleteWorkout)
+
+//UPDATE a new workout  
+router.patch('/:id', updateWorkout)
+
+
+module.exports = router

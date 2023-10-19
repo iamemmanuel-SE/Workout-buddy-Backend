606fc505b352f3648086fd85f283d663d060fa6a
iamemmanuel <fsdevmanuel@gmail.com>
2023-10-19 18:37:36 +0100

    changed cors URL

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const app = express()
 app.use(express.json())
 
 app.use(cors({
-    origin: "https://ghosttears.com/"
+    origin: "https://workoutmybuddy.com/"
 }));
 
 app.use((req, res, next) => {

b53824740e52f4988a99516e4bb1237151b35060
iamemmanuel <fsdevmanuel@gmail.com>
2023-10-19 22:14:25 +0100

    modified cors

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const app = express()
 app.use(express.json())
 
 app.use(cors({
-    origin: "https://workoutmybuddy.com/"
+    origin: "https://workoutmybuddy.onrender.com/"
 }));
 
 app.use((req, res, next) => {
